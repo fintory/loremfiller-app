96076882b12015a03fd5af61ab18b448e8fc49be
Dominik Schmidt <visualcake@me.com>
2015-10-18 11:19:30 +0200

    hide dock before ready

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var app         = require('app');
 var Tray        = require('tray');
 
 require('crash-reporter').start();
+app.dock.hide();
 
 app.on('ready', function(){
   var contextMenu = require('./menu');
   var appIcon = new Tray('./assets/iconTemplate.png');
 
-  app.dock.hide();
   appIcon.setContextMenu(contextMenu(app));
 });
