7f5cb4788e6ca43c647ff47dadca43d46c728ba5
Dominik Schmidt <visualcake@me.com>
2015-10-18 10:45:20 +0200

    remove comments

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Electron dependencies
 var app         = require('app');
 var Tray        = require('tray');
 
 app.on('ready', function(){
-  // Hide the app icon from the dock
   app.dock.hide();
 
   // Create new tray with specific icon template
+app.on('ready', function(){
   var contextMenu = require('./menu');
   var appIcon = new Tray('./assets/iconTemplate.png');
   appIcon.setContextMenu(contextMenu);
