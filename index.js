1633f4bfef500f62ee4952e822fa489e29407c29
Dominik Schmidt <visualcake@me.com>
2015-10-18 10:45:31 +0200

    move dock hide

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var app         = require('app');
 var Tray        = require('tray');
 
 app.on('ready', function(){
-  app.dock.hide();
 
   // Create new tray with specific icon template
 app.on('ready', function(){
   var contextMenu = require('./menu');
   var appIcon = new Tray('./assets/iconTemplate.png');
+
+  app.dock.hide();
   appIcon.setContextMenu(contextMenu);
 });
