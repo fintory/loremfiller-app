b7541a77f12e7ba0ac58b8e314e56364bbe3d617
Dominik Schmidt <visualcake@me.com>
2015-10-18 11:19:42 +0200

    fix tray icon path

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var app         = require('app');
 var Tray        = require('tray');
+var join        = require('path').join;
 
 require('crash-reporter').start();
 app.dock.hide();
 
 app.on('ready', function(){
   var contextMenu = require('./menu');
-  var appIcon = new Tray('./assets/iconTemplate.png');
+  var appIcon = new Tray(join(__dirname, 'assets/iconTemplate.png'));
 
   appIcon.setContextMenu(contextMenu(app));
 });
