74e429d5db0afea98a98626ae520a266c6e287e3
Dominik Schmidt <visualcake@me.com>
2015-10-18 10:45:01 +0200

    move menu require into ready event

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Electron dependencies
 var app         = require('app');
 var Tray        = require('tray');
-var contextMenu = require('./menu');
 
 app.on('ready', function(){
   // Hide the app icon from the dock
   app.dock.hide();
 
   // Create new tray with specific icon template
+  var contextMenu = require('./menu');
   var appIcon = new Tray('./assets/iconTemplate.png');
   appIcon.setContextMenu(contextMenu);
 });
